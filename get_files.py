5ba13c9dee1169c14edb5670a13bbb1b255a92ca
fatemehri <52169207+fatemehri@users.noreply.github.com>
2020-10-12 12:50:55 +0330

    Update get_files.py

--- get_files.py
+++ get_files.py
@@ -8,23 +8,21 @@ import glob
 def get_files():
     path = 'livestock/'
     ftp = FTP()
-    ftp.connect('partow.rdps.ir', 5021)
-    ftp.login('admin', 'A@123456')
+    ftp.connect('', 5021)
+    ftp.login('admin', '')
     ftp.cwd(path)
     ftp.pwd()
     files_name = ftp.nlst()
 
     for name in files_name:
-        print(name)
         ftp.retrbinary("RETR " + name, open(name, 'wb').write)
 
 
 def dec_files():
     bufferSize = 64 * 1024
-    password = "foopassword"
+    password = ""
     for name in glob.glob('*.aes'):
-        print(name)
-        pyAesCrypt.decryptFile(name, "dw_ftp_files/dec_files/{}.csv".format(name.split('.')[0]), password, bufferSize)
+        pyAesCrypt.decryptFile(name, "dec_files/{}.csv".format(name.split('.')[0]), password, bufferSize)
 
 
 if __name__ == "__main__":
